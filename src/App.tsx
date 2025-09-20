3b7e01d3ae1cbaad4cdc95e51f7aab05cd0a3149
Minsung Kang <minsungkang2012@gmail.com>
2025-09-20 11:41:46 +0900

    1. Practiced adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-export default function App(){
-  return(
-    <>
-      <div>Hello</div>
-    </>
+import Header from './components/Header';
+import Main from './components/Main';
+
+export default function App(){  // 여기에만 export default
+    return(
+        <>
+            <Header />  {/* 컴포넌트 사용 */}
+            <Main />
+        </>
   )
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <>
+            <h1>This is the main contents</h1>
+        </>
+    )
+}
